a39bc040ad31a2caa6f9b08415f884cbc55883d8
D R Ward <scdrw@login2.arc3.leeds.ac.uk>
2020-03-23 12:10:46 +0000

    Adding file counter.

--- postProcess.py
+++ postProcess.py
@@ -1,34 +1,71 @@
 #Link to path in which postProcess_lib is stored.
 import sys
 import os
-sys.path.insert(1,'/home/cserv1_a/soc_pg/scdrw/Documents/nbudocuments/PhD/SimNumerics/Python/postProcessingLib')
-import postProcess_lib as pp
+print(' ')
+# Counting number of files in directory.
+files, other = os.walk(".",topdown=True)
+file_count = len([f for f in files[2] if f[:20] == "plume_v1_production0"])
+print('Number of files is ',file_count,'.')
+# Number of files to skip each simulation.
+jump = 500
+print('Skip every ',jump,' files.')
+# Computes number of calculations to perform.
+to_calculate = int(round(float(file_count)/float(jump),0))
+print('Performing ',to_calculate,' calculations.')
+
+sys.path.insert(1,'/home/home01/scdrw/Python/scripts')
+#import compute_outline as co
+import make_videos as mv
 
 # Chooses which postprocessing script to run.
-# 0 - PseudoColourPlotting
-# 1 - integrateDomain
-# 2 - integratePlume
+switch = 2
 
-switch = 0 
+def umbrellaOutline():
+        co.umbrellaOutline('plume_v1_production',
+        8,      # Order 
+        3,      # Dimension
+        1,      # Start file
+        jump,      # Jump
+        file_count,      # Final timestep
+        1      # Image on/off
+        )
+        return
 
-def outline():
-	pp.computeOutline( 'plume_v1_production', 
-	7,	# Order 
-	3, 	# Dimension
-	1, 	# Start file
-	1, 	# Jump
-	2, 	# Final timestep
-	)
-	return	
-	
+def plumeOutline():
+        co.plumeOutline('plume_v1_production',
+        8,      # Order 
+        3,      # Dimension
+        1,      # Start file
+        jump,      # Jump
+        file_count,      # Final timestep
+        34,     # Number of elements in z-direction
+        0.07,   # Cutoff value for s
+        0,      # start averaging at this time
+        0      # Image on/off
+        )
+        return
+
+def makeVideo():
+        mv.pseudoColour('plume_v1_production',
+        8,      # Order 
+        3,      # Dimension
+        1,      # Start file
+        jump,      # Jump
+        file_count,      # Final timestep
+        )
+        return
+
+
 def choose_function(argument):
-	switcher = {
-		0: outline,
-	}
+        switcher = {
+                0: umbrellaOutline,
+                1: plumeOutline,
+                2: makeVideo,
+        }
+        # Get the function from switcher dictionary
+        func = switcher.get(argument)
 
-	# Get the function from switcher dictionary
-	func = switcher.get(argument)
-
-	return func()
+        return func()
 
 choose_function(switch)
+
